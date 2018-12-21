f6cf4008af35fc49301a94f41404629b1a55b1fa
JohnnyVim <32849404+JohnnyVim@users.noreply.github.com>
2018-12-21 12:44:30 +0100

    script

--- test.sh
+++ test.sh
@@ -1,47 +1,32 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NOCOLOR='\033[0m'
+username=$1
+rootdisk=$2
 
-function pass {
-	echo -e "\r[${GREEN}PASS${NOCOLOR}] $1"
-}
-
-function error {
-	echo -e "\r[${RED}ERROR${NOCOLOR}] $1"
-	exit
-}
-
-if [[ `id -u` -eq 0 ]]; then
-  pass 'root'
+if [[ $(id -u) -eq 0 ]]; then
+  echo root
 else
-  error 'this script has to be run as root'
+  echo 'this script has to be run as root'
+  exit 1
 fi
 
 if find "/sys/firmware/efi/efivars" -mindepth 1 -print -quit | grep -q .; then
-  pass 'UEFI boot'
+  echo 'UEFI boot'
 else
-  error 'not booted in uefi mode'
+  echo 'not booted in uefi mode'
+  exit 1
 fi
 
-#echo -n '[TEST] internet connection'
-#if ping archlinux.org -c 3 &> /dev/null; true; then
-#  pass 'internet connection'
-#else
-#  error 'no working internet connection'
-#fi
+echo -n '[TEST] internet connection'
+if
+  ping archlinux.org -c 3 &>/dev/null
+  true
+then
+  echo 'internet connection'
+else
+  echo 'no working internet connection'
+  exit 1
+fi
 
-#username=`dialog --nocancel --inputbox 'enter username' 8 50 3>&1 1>&2 2>&3 3>&1`
-#echo $username
-disks=(`lsblk | tail -n +2 | cut -d ' ' -f 1`)
-options=
-for ((i = 0; i < ${#disks[@]}; i++)); do
-	options="$options $i ${disks[i]} off "
-done
-menoptions=(${options})
-tess=(dialog --separate-output --nocancel --checklist 'testtt' 40 150 20)
-te=$("${tess[@]}" "${menoptions[@]}" 2>&1 >/dev/tty)
-for d in $te; do
-	echo $d
-done
+echo formatting disk $rootdisk
+echo setting up use $username

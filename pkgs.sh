e4484440294464261650218c0aeccb68285d31e7
JohnnyVim <32849404+JohnnyVim@users.noreply.github.com>
2018-12-22 09:20:54 +0100

    update

--- pkgs.sh
+++ pkgs.sh
@@ -3,20 +3,13 @@
 mkdir .build
 cd .build
 
-function pacins() {
-  pacman -S --noconfirm --needed $@
-}
-
-function yayins() {
-  yay -S --noconfirm --needed $@
-}
-
 curl -sL "https://raw.githubusercontent.com/JohnnyVim/ArchCustomInstall/master/makepkg.conf" -o /etc/makepkg.conf
-pacins git go
+pacman -Sy --noconfirm --needed git go
 
 git clone https://aur.archlinux.org/yay.git
 chown -R 1000:users .
 cd yay
 sudo -u \#1000 makepkg
-pacman -U yay*.pkg.tar.xz
+pacman -U --noconfirm --needed yay*.pkg.tar.xz
 
+curl -sL "https://raw.githubusercontent.com/JohnnyVim/ArchCustomInstall/master/pkgs" | pacman -S --noconfirm --needed

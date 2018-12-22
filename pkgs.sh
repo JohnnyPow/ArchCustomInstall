1e73bef1345738903e1e21029f564c4309e5c022
JohnnyVim <32849404+JohnnyVim@users.noreply.github.com>
2018-12-22 21:13:50 +0100

    update

--- pkgs.sh
+++ pkgs.sh
@@ -3,6 +3,11 @@
 CYAN="\033[0;36m"
 NOCOLOR="\033[0m"
 IFS=$'\n'
+
+function info() {
+  echo -e "\r[${CYAN}INFO${NOCOLOR}] $1"
+}
+
 function infon() {
   echo -en "\r                                                                                \r[${CYAN}INFO${NOCOLOR}] $1"
 }
@@ -19,20 +24,23 @@ cd yay
 sudo -u \#1000 makepkg &>/dev/null
 pacman -U --noconfirm --needed yay*.pkg.tar.xz &>/dev/null
 
+info "installing official packages"
 curl -sL "https://raw.githubusercontent.com/JohnnyVim/ArchCustomInstall/master/pkgs" | pacman -S --noconfirm --needed - &>out &
-while [ $(ps | grep pacman | wc -l) -gt 0 ]; do
-  infon "$(cat out | tail -n 1 | sed "s/\-[^-]*-[^-]*-[^-]*$/.../g")"
+while [ $(ps --no-headers -C pacman | wc -l) -gt 0 ]; do
+  infon "$(cat out | tail -n 1)"
   sleep 1
 done
 echo
+info "installing AUR packages"
 curl -sL "https://raw.githubusercontent.com/JohnnyVim/ArchCustomInstall/master/aurpkgs" | sudo -u \#1000 yay -S --noconfirm --needed - &>out &
-while [ $(ps | grep yay | wc -l) -gt 0 ]; do
-  infon "$(cat out | tail -n 1 | sed "s/\-[^-]*-[^-]*-[^-]*$/.../g")"
+while [ $(ps --no-headers -C yay | wc -l) -gt 0 ]; do
+  infon "$(cat out | tail -n 1)"
   sleep 1
 done
 echo
 systemctl enable lightdm &>/dev/null
 curl -sL "https://raw.githubusercontent.com/JohnnyVim/ArchCustomInstall/master/lightdm.conf" -o /etc/lightdm/lightdm.conf
+info "installing st"
 git clone git://git.suckless.org/st &>/dev/null
 cd st
 make &>/dev/null && make install &>/dev/null
